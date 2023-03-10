6a953306f12c7f892cf217cf25a6403c8de0d5cb
AllanVinsmoke <125388685+Allan-Drip@users.noreply.github.com>
2023-03-10 14:45:57 -0300

    Add files via upload

---
+++ ListaResolvida/q_05/src/br/edu/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		System.out.println("Esse programa mostrará o valor do seu salário após o aumento sofrido pelo mesmo. \n");
+		
+		double sal;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o valor do seu salário atual: ");
+		sal = sc.nextDouble();
+		
+		double perc;
+		Scanner pc = new Scanner(System.in);
+		System.out.println("Digite o valor do aumento da porcentagem do salário: ");
+		perc = pc.nextDouble();
+		
+		double novosal;
+		novosal = sal * ((100+perc)/100);
+		System.out.println("O novo valor do salário é de: " +novosal+ " reais");
+		
+	}
+}
