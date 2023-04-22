c8961f3b5e5738c2375db337c2e29d57d27877e7
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 19:06:19 -0300

    Update Principal.java

--- ListaResolvida/q_16/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_16/src/br/edu/principal/Principal.java
@@ -1,24 +1,30 @@
 package br.edu.principal;
 import java.util.Scanner;
-public class Principal {
-	public static void main(String[] args) {
-		
-		System.out.println("======================================================================================================");
-		System.out.println("Mostrador do total de convites que devem ser vendidos para arcar com o custo da produÁ„o do espet·culo!");
-		System.out.println("======================================================================================================\n");
 
-		double custo = 0;
-		double convite = 0;
-		double quantidade = 0;
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite o valor do custo do espet·culo: ");
-		custo = sc.nextDouble();
-		
-		System.out.println("Digite o valor do convite do espet·culo: ");
-		convite = sc.nextDouble();
-		
-		quantidade = custo / convite;
-		System.out.println("O total de convites que devem ser vendidos para arcar com o custo da produÁ„o do espet·culo È: " + quantidade);
-	}
+public class Principal {
+    public static void main(String[] args) {
+        
+        System.out.println("===========================================================");
+        System.out.println("Calculadora de sal√°rio baseado no n√∫mero de horas trabalhadas");
+        System.out.println("===========================================================\n");
+
+        double horasTrabalhadas;
+        double salarioMinimo;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o n√∫mero de horas trabalhadas: ");
+        horasTrabalhadas = sc.nextDouble();
+        
+        System.out.println("Digite o valor do sal√°rio m√≠nimo: ");
+        salarioMinimo = sc.nextDouble();
+        
+        double valorHoraTrabalhada = salarioMinimo / 2;
+        double salarioBruto = horasTrabalhadas * valorHoraTrabalhada;
+        double imposto = salarioBruto * 0.03;
+        double salarioLiquido = salarioBruto - imposto;
+        
+        System.out.println("O sal√°rio l√≠quido a receber √© de: R$" + salarioLiquido);
+    }
 }
+
