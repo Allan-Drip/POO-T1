81fa240db7e20761742f42fce5570304479215e6
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 19:08:25 -0300

    Update Principal.java

--- ListaResolvida/q_17/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_17/src/br/edu/principal/Principal.java
@@ -4,18 +4,18 @@ public class Principal {
 public static void main(String[] args) {
 		
         	 System.out.println("==========================================================");
-        	 System.out.println("Calculo de um deposito menos um imposto de cada de cheque ");   
+        	 System.out.println("C√°lculo de um dep√≥sito menos um imposto de cada de cheque ");   
         	 System.out.println("==========================================================\n");
 
                  Scanner sc = new Scanner(System.in); 
                  
                  double salario,cheque1,cheque2,cf1,cf2,saldo; 
                  
-                 System.out.println("Digite o valor do seu sal·rio: "); 
+                 System.out.println("Digite o valor do seu sal√°rio: "); 
                  salario = sc.nextDouble(); 
                  
                  
-                 System.out.println("Digite o valor do depÛsito: "); 
+                 System.out.println("Digite o valor do dep√≥sito: "); 
                  saldo = sc.nextDouble(); 
                  
                  System.out.println("Digite o valor do cheque1: "); 
@@ -27,7 +27,7 @@ public static void main(String[] args) {
                  cf1 = cheque1 * 0.0038; 
                  cf2 = cheque2 * 0.0038; 
                  saldo = salario - cheque1 - cheque2 - cf1 - cf2; 
-                 System.out.println("O valor do saldo atual È de: "+(saldo)); 
+                 System.out.println("O valor do saldo atual √© de: "+(saldo)); 
          } 
   
  }
