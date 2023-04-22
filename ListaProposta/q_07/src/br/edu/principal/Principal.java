4a3c98110970d2d20b25acc408db2ac72834b442
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 18:08:35 -0300

    Update Principal.java

--- ListaProposta/q_07/src/br/edu/principal/Principal.java
+++ ListaProposta/q_07/src/br/edu/principal/Principal.java
@@ -1,12 +1,12 @@
 package br.edu.principal; 
-  import java.util.Scanner; 
+import java.util.Scanner; 
   
  public class Principal { 
  
          public static void main(String[] args) { 
         	 
         	 System.out.println("=================================================================");
-        	 System.out.println("Vari�ncia da sua massa corporal em 15 % (para mais ou para menos)");
+        	 System.out.println("Variância da sua massa corporal em 15 % (para mais ou para menos)");
         	 System.out.println("=================================================================\n");
 
          Scanner sc = new Scanner(System.in); 
@@ -15,12 +15,12 @@ package br.edu.principal;
          System.out.println("Digite o seu peso(em quilos): "); 
          peso = sc.nextDouble(); 
           
-         System.out.println("O seu peso atual � "+peso+" quilos."); 
+         System.out.println("O seu peso atual é de "+peso+" quilos."); 
           
          double engo = ((15 * peso)/100) + peso; 
          double emag = peso - ((20 * peso)/100); 
           
-         System.out.println("Se voc� engordar 15% do seu peso ficar� com "+engo+" quilos."); 
-         System.out.println("Se voc� emagrecer 20% do seu peso ficar� com "+emag+" quilos."); 
+         System.out.println("Se você engordar 15% do seu peso ficará com "+engo+" quilos."); 
+         System.out.println("Se voc� emagrecer 20% do seu peso ficará com "+emag+" quilos."); 
          } 
  }
