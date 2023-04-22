2f76f80e1c96241c91269df8acbb2557ec2bd363
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 18:20:55 -0300

    Update Principal.java

--- ListaProposta/q_20/src/br/edu/principal/Principal.java
+++ ListaProposta/q_20/src/br/edu/principal/Principal.java
@@ -7,17 +7,17 @@ public class Principal {
          public static void main(String[] args) { 
         	 
         	 System.out.println("===================================================="); 
-        	 System.out.println("C�lculo do comprimento de uma escada apoiada no ch�o"); 
+        	 System.out.println("Cálculo do comprimento de uma escada apoiada no chão"); 
         	 System.out.println("====================================================\n"); 
         	 
                    Scanner sc = new Scanner(System.in); 
                   
                  Double dist = 0.0; 
-                 System.out.print("Digite a dist�ncia da escada para a parede: "); 
+                 System.out.print("Digite a distância da escada para a parede: "); 
                  dist = sc.nextDouble(); 
                   
                  Double ang = 0.0; 
-                 System.out.print("Digite o �ngulo formado entre a escada e o ch�o: "); 
+                 System.out.print("Digite o ângulo formado entre a escada e o chão: "); 
                  ang = sc.nextDouble(); 
                   
                  Double radiano = ang * Math.PI / 180; 
