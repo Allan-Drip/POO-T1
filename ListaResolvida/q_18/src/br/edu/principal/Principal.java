5bdd131fb80deaa12c2b73e6ec752b5608352552
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 19:12:56 -0300

    Update Principal.java

--- ListaResolvida/q_18/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_18/src/br/edu/principal/Principal.java
@@ -6,27 +6,27 @@ package br.edu.principal;
          public static void main(String[] args) { 
         	 
         	 System.out.println("=============================================================="); 
-        	 System.out.println("Calculo de quanto restar· de raÁ„o para dois gatos apÛs 5 dias"); 
+        	 System.out.println("Calculo de quanto restar√° de ra√ß√£o para dois gatos ap√≥s 5 dias"); 
         	 System.out.println("==============================================================\n"); 
 
                  Scanner sc = new Scanner(System.in); 
                  
                  double peso_saco,racao_gato1,racao_gato2,totalfinal; 
                  
-                 System.out.println("Digite o peso total do saco de raÁ„o em Kg: "); 
+                 System.out.println("Digite o peso total do saco de ra√ß√£o em Kg: "); 
                  peso_saco = sc.nextDouble(); 
                  
-                 System.out.println("Digite a quantidade em gramas dar· ao gato1: "); 
+                 System.out.println("Digite a quantidade em gramas dar√° ao gato 1 por dia: "); 
                  racao_gato1 = sc.nextDouble(); 
                  
                  racao_gato1 = racao_gato1 / 1000; 
-                 System.out.println("Digite a quantidade em gramas dar· ao gato2: "); 
+                 System.out.println("Digite a quantidade em gramas dar√° ao gato 2 por dia: "); 
                  racao_gato2 = sc.nextDouble(); 
                  
                  racao_gato2 = racao_gato2 / 1000; 
                  
                  totalfinal = peso_saco - 5*(racao_gato1 + racao_gato2); 
-                 System.out.println("ApÛs 5 dias, restar· "+ totalfinal + " Kg de raÁ„o"); 
+                 System.out.println("Ap√≥s 5 dias, restar√° "+ totalfinal + " Kg de ra√ß√£o"); 
                   
          } 
   
