306511b0c0633506d73513927e9901c8ef516bd4
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 18:22:26 -0300

    Update Principal.java

--- ListaProposta/q_22/src/br/edu/principal/Principal.java
+++ ListaProposta/q_22/src/br/edu/principal/Principal.java
@@ -6,18 +6,18 @@ package br.edu.principal;
          public static void main(String[] args) { 
         	 
         	 System.out.println("=========================================================================================");
-        	 System.out.println("N�mero de diagonais de um pol�gono convexo regular com base no n�mero de lados do mesmo. ");
+        	 System.out.println("Número de diagonais de um polígono convexo regular com base no número de lados do mesmo. ");
         	 System.out.println("=========================================================================================\n");
         	 
                  Scanner sc = new Scanner(System.in); 
                  
                  int n,nd; 
                  
-                 System.out.println("Digite o n�mero de lados do poligono: "); 
+                 System.out.println("Digite o número de lados do poligono: "); 
                  n = sc.nextInt(); 
                  
                  nd = n * (n-3)/2;
-                 System.out.println("O n�mero de diagonais desse poligono �: "+nd); 
+                 System.out.println("O número de diagonais desse poligono é: "+nd); 
          } 
   
  }
