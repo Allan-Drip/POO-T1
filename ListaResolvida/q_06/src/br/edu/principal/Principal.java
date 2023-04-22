deec89077853eeb1e14a1f2c60840b941aad1ce4
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 18:59:08 -0300

    Update Principal.java

--- ListaResolvida/q_06/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_06/src/br/edu/principal/Principal.java
@@ -5,18 +5,21 @@ public class Principal {
     
 	public static void main(String[] args) {
 		
-		System.out.println("Este programa mostrar· o valor do seu sal·rio apÛs as mudanÁas sofridas pelo mesmo. \n");
+		System.out.println("***********************************************************");
+		System.out.println("O valor do seu sal√°rio ap√≥s as mudan√ßas sofridas pelo mesmo");
+		System.out.println("***********************************************************\n");
+
 
 		double sal;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite o valor do seu sal·rio: ");
+		System.out.println("Digite o valor do seu sal√°rio: ");
 		sal = sc.nextDouble();
 		
 		 double grat = sal * 1.05;
 		 double imp = sal * 1.07;
 		 float salreceber = (float) (sal + grat - imp);
 		 
-		 System.out.println("O valor final do sal·rio ser· de: " +salreceber);
+		 System.out.println("O valor final do sal√°rio ser√° de: " +salreceber);
 		
 	}
 }
