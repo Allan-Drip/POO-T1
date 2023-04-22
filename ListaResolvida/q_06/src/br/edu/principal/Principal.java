cc8443b67c75bf82dca7bdf8b3eca4cb23db2bd3
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 19:00:41 -0300

    Update Principal.java

--- ListaResolvida/q_06/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_06/src/br/edu/principal/Principal.java
@@ -1,25 +1,24 @@
 package br.edu.principal;
 import java.util.Scanner;
+
 public class Principal {
 
-    
-	public static void main(String[] args) {
-		
-		System.out.println("***********************************************************");
-		System.out.println("O valor do seu salário após as mudanças sofridas pelo mesmo");
-		System.out.println("***********************************************************\n");
+    public static void main(String[] args) {
 
+        System.out.println("***********************************************************");
+        System.out.println("O valor do seu salário após as mudanças sofridas pelo mesmo");
+        System.out.println("***********************************************************\n");
 
-		double sal;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite o valor do seu salário: ");
-		sal = sc.nextDouble();
-		
-		 double grat = sal * 1.05;
-		 double imp = sal * 1.07;
-		 float salreceber = (float) (sal + grat - imp);
-		 
-		 System.out.println("O valor final do salário será de: " +salreceber);
-		
-	}
+        double sal;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor do seu salário: ");
+        sal = sc.nextDouble();
+
+        double grat = sal * 0.05;
+        double imp = sal * 0.07;
+        double salreceber = sal + grat - imp;
+
+        System.out.println("O valor final do salário será de: " + salreceber);
+    }
 }
+
